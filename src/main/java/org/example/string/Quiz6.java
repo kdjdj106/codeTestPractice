9dc9a501c79f8be604beb77bf5c8691fe24df409
kdjdj106 <kdjdj106@naver.com>
2024-02-09 01:53:22 +0900

    init

---
+++ src/main/java/org/example/string/Quiz6.java
@@ -0,0 +1,17 @@
+package org.example.string;
+
+import java.util.Scanner;
+
+public class Quiz6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i){
+                sb.append(str.charAt(i));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
---
+++ src/main/java/org/example/twoPointer_SlidingWindow/Quiz1.java
@@ -0,0 +1,55 @@
+package org.example.twoPointer_SlidingWindow;
+/*
+
+두 배열 합치기
+        오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램
+        을 작성하세요.
+        ▣ 입력설명
+        첫 번째 줄에 첫 번째 배열의 크기 N(1<=N<=100)이 주어집니다.
+        두 번째 줄에 N개의 배열 원소가 오름차순으로 주어집니다.
+        세 번째 줄에 두 번째 배열의 크기 M(1<=M<=100)이 주어집니다.
+        네 번째 줄에 M개의 배열 원소가 오름차순으로 주어집니다.
+        각 리스트의 원소는 int형 변수의 크기를 넘지 않습니다.
+        ▣ 출력설명
+        오름차순으로 정렬된 배열을 출력합니다.
+        ▣ 입력예제 1
+        3
+        1 3 5
+        5
+        2 3 6 7 9
+        ▣ 출력예제 1
+        1 2 3 3 5 6 7 9
+*/
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Quiz1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+        int m = kb.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = kb.nextInt();
+        }
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1=0, p2=0;
+        while (p1<n && p2<m){
+            if(a[p1]<b[p2]) answer.add(a[p1++]);
+            else answer.add(b[p2++]);
+        }
+        while(p1<n) answer.add(a[p1++]);
+        while(p2<m) answer.add(b[p2++]);
+
+        for (Integer num : answer){
+            System.out.print(num+" ");
+        }
+    }
+}
+
