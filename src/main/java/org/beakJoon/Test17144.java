7f26e90dd339d7f17991dc97d4c2ab3a36889289
KimDongJoon <kdjdj106@naver.com>
2024-08-12 14:24:18 +0900

    init

---
+++ src/main/java/org/beakJoon/Test17144.java
@@ -0,0 +1,78 @@
+package org.beakJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Test17144 {
+    static int n, m, time;
+    static int[][] board;
+    static int[][] tmpBoard;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int airFresherUp, airFresherDown;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        time = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 1; j++) {
+                // 공기청정기 위치 찾기
+                if (board[i][j] == -1) {
+                    airFresherUp = i;
+                    airFresherDown = i + 1;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < time; i++) {
+            tmpBoard = new int[n][m];
+            // 미세먼지 확산
+            diffusionDust();
+            board = tmpBoard.clone();
+            // 공기청정기로 공기이동
+            moveDust();
+        }
+
+    }
+
+    static void diffusionDust() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == -1) {
+                    tmpBoard[i][j] = -i;
+                    continue;
+                }
+                tmpBoard[i][j] = board[i][j];
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                        tmpBoard[nx][ny] += board[nx][ny] / 5;
+                        tmpBoard[i][j] -= board[i][j] / 5;
+                    }
+                }
+            }
+        }
+    }
+
+    static void moveDust() {
+        // 역으로 공기청정기로 들어오는 방향부터 로직을 돌릳다.
+        // 그래야만 먼지가 이동했을 때 한칸이 비게 되고 그 빈자리로 먼지가 이동하게 된다.
+
+    }
+}
