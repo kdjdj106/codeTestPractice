35d78f1cea5c1a86d1498a2033859a59780fe6be
KimDongJoon <kdjdj106@naver.com>
2024-08-16 19:01:12 +0900

    init

--- src/main/java/org/beakJoon/Test103.java
+++ src/main/java/org/beakJoon/Test103.java
@@ -3,42 +3,39 @@ package org.beakJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Test103 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());	//히스토그램 개수
-        int[] height = new int[N+2];			//히스토그램 높이
-
-        long answer = 0;
-        Stack<Integer> stack = new Stack<>();		//히스토그램 인덱스 담을 스택
-
-        height[0] = 0;	//맨 첫번째 히스토그램까지 포함해서 계산할 때 편리하게
-        height[N] = 0;	//반복문 마지막에 스택에 남은 애들을 계산해주기 위해
-
-        for(int i = 1; i <= N; i++)
-            height[i] = Integer.parseInt(br.readLine());
-
-        stack.push(0);
-
-        for(int i = 1; i <= N+1; i++) {
-            while(!stack.isEmpty()) {
-                int top = stack.peek();
-
-                if(height[top] <= height[i])
-                    break;
-
-                stack.pop();
-                answer = Math.max(answer, height[top] * (i-stack.peek()-1));
-            }
-
-            stack.push(i);
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
+        int answer = 0;
+        int lp = 0;
+        int rp = 1;
 
+        while (rp <= n) {
+            if (rp == n) {
+                for (int i = lp; i < rp; i++) {
+                    answer = Math.max(answer, arr[lp] * (rp - i));
+                    lp++;
+                }
+                break;
+            } else if (arr[rp - 1] <= arr[rp]) {
+                rp++;
+                continue;
+            } else {
+                for (int i = lp; i < rp; i++) {
+                    answer = Math.max(answer, arr[lp] * (rp - i));
+                    lp++;
+                }
+                lp = rp;
+            }
+            rp++;
+        }
         System.out.println(answer);
-
     }
 
 }
