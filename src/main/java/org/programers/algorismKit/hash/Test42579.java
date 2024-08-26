da01e4133795c9b5c5dfdf46e9c4f03dad80b7c2
kdjdj106 <kdjdj106@gmail.com>
2024-08-27 03:56:54 +0900

    init

--- src/main/java/org/programers/algorismKit/hash/Test42579.java
+++ src/main/java/org/programers/algorismKit/hash/Test42579.java
@@ -1,39 +1,55 @@
 package org.programers.algorismKit.hash;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Test42579 {
     public static void main(String[] args) {
         String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] plays ={500, 600, 150, 800, 2500};
+        int[] plays = {500, 600, 150, 800, 2500};
 
-        solution(genres, plays);
+        System.out.println(Arrays.toString(solution(genres, plays)));
     }
+
     static public int[] solution(String[] genres, int[] plays) {
-        int[] answer = new int [plays.length];
+        int n = genres.length;
         HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < genres.length; i++) {
+        for (int i = 0; i < n; i++) {
             map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
         }
-        ArrayList<String> list = new ArrayList<>();
-        for (String genre : genres) {
-            list.add(genre);
+        ArrayList<String> genreList = new ArrayList<>();
+        for (HashMap.Entry<String, Integer> entry : map.entrySet()) {
+            genreList.add(entry.getKey());
         }
-        // ArrayList<String> list = new ArrayList<>(Arrays.asList(genres)); 위 로직을 이렇게 한줄로 정의 할수도 있다.
-        // 플레이한 횟수로 내림차순
-        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
-        return answer;
+        genreList.sort((o1, o2) -> map.get(o2) - map.get(o1));
+
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (String str : genreList) {
+            ArrayList<Song> songList = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                if (str.equals(genres[i])) {
+                    songList.add(new Song(i, plays[i]));
+                }
+            }
+            Collections.sort(songList);
+            answer.add(songList.get(0).idx);
+            if (songList.size() > 1) answer.add(songList.get(1).idx);
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 
     static class Song implements Comparable<Song> {
         @Override
         public int compareTo(Song o) {
-            return o.plays - this.;
+            if (o.plays == this.plays) return this.idx - o.idx;
+            return o.plays - this.plays;
         }
 
         int idx, plays;
-        public Song(int idx, int plays){
+
+        public Song(int idx, int plays) {
             this.idx = idx;
             this.plays = plays;
         }
