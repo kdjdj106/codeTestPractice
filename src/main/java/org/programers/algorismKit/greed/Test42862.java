b97ca8ab7051b6a0265fe1564e7c052589b3acd5
KimDongJoon <kdjdj106@naver.com>
2024-09-02 14:39:39 +0900

    init

---
+++ src/main/java/org/programers/algorismKit/greed/Test42862.java
@@ -0,0 +1,32 @@
+package org.programers.algorismKit.greed;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/42862
+체육복
+* */
+public class Test42862 {
+    public static void main(String[] args) {
+        int n =5;
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        solution(n, lost, reserve);
+    }
+    static public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]){
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
