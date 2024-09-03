b41db3d1c3d05add7800cfbd4bdeee4f6a5f3d60
KimDongJoon <kdjdj106@naver.com>
2024-09-03 13:48:40 +0900

    init

--- src/main/java/org/programers/algorismKit/greed/Test42862.java
+++ src/main/java/org/programers/algorismKit/greed/Test42862.java
@@ -1,6 +1,5 @@
 package org.programers.algorismKit.greed;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 /*
@@ -9,16 +8,30 @@ https://school.programmers.co.kr/learn/courses/30/lessons/42862
 * */
 public class Test42862 {
     public static void main(String[] args) {
-        int n =5;
+        int n = 5;
         int[] lost = {2, 4};
         int[] reserve = {1, 3, 5};
-        solution(n, lost, reserve);
+        System.out.println(solution(n, lost, reserve));
     }
+
     static public int solution(int n, int[] lost, int[] reserve) {
         int answer = n - lost.length;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
         for (int i = 0; i < lost.length; i++) {
             for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]){
+                if (lost[i] == reserve[j]) {
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (reserve[j] == lost[i]-1 || reserve[j] == lost[i]+1) {
                     answer++;
                     lost[i] = -1;
                     reserve[j] = -1;
